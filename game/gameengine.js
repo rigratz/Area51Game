375c02fe7c4bd8e5e69e1a3727bafc6250585510
Riley Gratzer <rigratz@uw.edu>
2016-02-03 17:00:15 -0800

    CAMERAAAAAAAgit add .git add .

--- game/gameengine.js
+++ game/gameengine.js
@@ -131,6 +131,10 @@ GameEngine.prototype.addEntity = function (entity) {
 GameEngine.prototype.draw = function () {
     this.ctx.clearRect(0, 0, this.surfaceWidth, this.surfaceHeight);
     this.ctx.save();
+    if (this.camera != null) {
+      console.log("TRANSFORMING");
+      this.ctx.translate(-this.camera.xView, -this.camera.yView);
+    }
     for (var i = 0; i < this.entities.length; i++) {
         this.entities[i].draw(this.ctx);
     }
