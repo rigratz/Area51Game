518e98837c153a81e588878c92c4029901329645
shmurphy <shmurphy@uw.edu>
2016-03-09 21:24:09 -0800

    merged

--- js/gameengine.js
+++ js/gameengine.js
@@ -16,7 +16,7 @@ function GameEngine() {
     this.hasSpeed = false;
 
     this.treeBossDead = false;
-
+    this.snakeBossDead = false;
     this.hasBulletUpgrade = false;
 
     this.hasShotgun = false;
@@ -83,9 +83,11 @@ GameEngine.prototype.init = function (ctx) {
 }
 
 GameEngine.prototype.generateWorlds = function() {
-    this.worlds["Area 51"] = new World("Area 51", this);
-    this.worlds["World 1"] = new World("World 1", this);
-    this.worlds["World 2"] = new World("World 2", this);
+  this.worlds["Area 51"] = new World("Area 51", this);
+  this.worlds["World 1"] = new World("World 1", this);
+  this.worlds["World 2"] = new World("World 2", this);
+  this.worlds["World 3"] = new World("World 3", this);
+  //this.worlds["Final Boss"] = new World("Final Boss", this);
 }
 GameEngine.prototype.switchWorlds = function(comingFrom, goingTo) {
     //console.log("PORTAL");
@@ -180,6 +182,136 @@ GameEngine.prototype.setLevel = function(exitedFrom) {
     var ch;
     for (var i = 0; i < currLevel.grid[0].length; i++) {
         for (var j = 0; j < currLevel.grid.length; j++) {
+// <<<<<<< HEAD
+//               ch = currLevel.grid[j][i];
+//
+//           /************************
+//            * Player related symbols
+//            ************************/
+//               if (ch === "playernorth" && exitedFrom === "south") {
+//                     var player = new PlayerOne(this, i * 50, j * 50 - 52, AM.getAsset("./js/img/playerv2.png"));
+//                     this.addEntity(player);
+//                     this.player = player;
+//                     this.camera.follow(player, 100, 100);
+//               } else if (ch === "playersouth" && exitedFrom === "north") {
+//                     var player = new PlayerOne(this, i * 50, j * 50 - 52, AM.getAsset("./js/img/playerv2.png"));
+//                     this.addEntity(player);
+//                     this.player = player;
+//                     this.camera.follow(player, 100, 100);
+//               } else if (ch === "playereast" && exitedFrom === "west") {
+//                     var player = new PlayerOne(this, i * 50, j * 50 - 52, AM.getAsset("./js/img/playerv2.png"));
+//                     this.addEntity(player);
+//                     this.player = player;
+//                     this.camera.follow(player, 100, 100);
+//               } else if (ch === "playerwest" && exitedFrom === "east") {
+//                     var player = new PlayerOne(this, i * 50, j * 50 - 52, AM.getAsset("./js/img/playerv2.png"));
+//                     this.addEntity(player);
+//                     this.player = player;
+//                     this.camera.follow(player, 100, 100);
+//               }
+//
+//           /************************
+//            * Enemy related symbols
+//            ************************/
+//               else if (ch === "bird") {
+//                     this.addEntity(new BirdEnemy(this, i * 50, j * 50, AM.getAsset("./js/img/bird_enemy_spritesheet.png"), 10));
+//               } else if (ch === "catbird") {
+//                     this.addEntity(new BirdEnemy(this, i * 50, j * 50, AM.getAsset("./js/img/grumpy_cat.png"), 2));
+//               } else if (ch === "smallcrazycat") {
+//                     this.addEntity(new CrazyCatEnemy(this, i * 50, (j * 50), AM.getAsset("./js/img/alien.png"), 0.6));
+//               } else if (ch === "idle_bird") {
+//                     this.addEntity(new BirdEnemy(this, i * 50, j * 50, AM.getAsset("./js/img/bird_enemy_spritesheet.png"),0));
+//               } else if (ch === "bigcrazycat") {
+//                     this.addEntity(new CrazyCatEnemy(this, i * 50, j * 50, AM.getAsset("./js/img/alien.png"), 1));
+//               } else if (ch === "dragon") {
+//                     this.addEntity(new Dragon(this, i * 50, j * 50, AM.getAsset("./js/img/dragon.png")));
+//               } else if (ch === "tree_boss" && !this.treeBossDead) {
+//                     this.addEntity(new TreeBoss(this, i * 50, j * 50, AM.getAsset("./js/img/boss.png"), 0));
+//               } else if (ch === "shadow_enemy") {
+//                     this.addEntity(new ShadowEnemy(this, i * 50, j * 50, AM.getAsset("./js/img/shadow_enemy.png"), 2));
+//               } else if (ch === "shadow_enemy_bound") {
+//                     this.addEntity(new ShadowEnemyBound(this, i * 50, j * 50));
+//               }
+//
+//           /************************
+//            * Upgrade related symbols
+//            ************************/
+//               else if (ch == "speedboost") {
+//                     if (!this.hasSpeed) this.addEntity(new PowerUp(AM.getAsset("./js/img/speed_upgrade_icon.png"), this, i * 50, j * 50, 50, 50, "S"));
+//               }
+//               else if (ch == "bullet_upgrade") {
+//                     if (!this.hasBulletUpgrade) this.addEntity(new PowerUp(AM.getAsset("./js/img/bullet_upgrade_icon.png"), this, i * 50, j * 50, 50, 50, "B"));
+//               }
+//               else if (ch == "shotgun") {
+//                     if (!this.hasShotgun) this.addEntity(new PowerUp(AM.getAsset("./js/img/multishot_icon.png"), this, i * 50, j * 50, 50, 50, "R"));
+//               }
+//               else if (ch == "doublejump") {
+//                   if (!this.hasDoublejump) this.addEntity(new PowerUp(AM.getAsset("./js/img/double_jump_icon.png"), this, i * 50, j * 50, 50, 50, "J"));
+//               }
+//               else if (ch === "healthpack") {
+//                   if (!this.usedHealth) this.addEntity(new HealthPack(AM.getAsset("./js/img/health_icon.png"), this, i * 50, j * 50, 50, 50));
+//               }
+//
+//           /************************
+//            * Level related symbols
+//            ************************/
+//                 else if (ch === "platform") {
+//                      var mult = 1;
+//                      while (j + mult < currLevel.grid.length && currLevel.grid[j+mult][i] === "platform") {
+//                        currLevel.grid[j+mult][i] = "used_platform";
+//                        mult += 1;
+//                      }
+//                      this.platforms.push((new Platform(AM.getAsset("./js/img/textures.png"), this, i * 50, j * 50, 50, 50 * mult, "X")));
+//                } else if (ch === "exit") {
+//                     var exitDir = null;
+//                     if (i === 0) {
+//                       exitDir = "west";
+//                     } else if (i === currLevel.grid[0].length - 1) {
+//                       exitDir = "east";
+//                     } else if (j === 0) {
+//                       exitDir = "north";
+//                     } else if (j === currLevel.grid.length - 1) {
+//                       exitDir = "south";
+//                     }
+//                     this.exits.push((new Exit(AM.getAsset("./js/img/textures.png"), this, i*50, j*50, 50, 50, "exit", exitDir)));
+//               } else if (ch === "used_platform") {
+//                   currLevel.grid[j][i] = "platform";
+//               } else if (ch === "portal0") {
+//                   this.exits.push(new Portal(AM.getAsset("./js/img/textures.png"), this, i*50, j*50, 50, 50, "portal", "Area 51"));
+//               } else if (ch === "portal1") {
+//                   this.exits.push(new Portal(AM.getAsset("./js/img/textures.png"), this, i*50, j*50, 50, 50, "portal", "World 1"));
+//               } else if (ch === "portal2") {
+//                   this.exits.push(new Portal(AM.getAsset("./js/img/textures.png"), this, i*50, j*50, 50, 50, "portal", "World 2"));
+//               } else if (ch === "portal3") {
+//                   this.exits.push(new Portal(AM.getAsset("./js/img/textures.png"), this, i*50, j*50, 50, 50, "portal", "World 3"));
+//               } else if (ch === "portal4") {
+//                   this.exits.push(new Portal(AM.getAsset("./js/img/textures.png"), this, i*50, j*50, 50, 50, "portal", "Final Boss"));
+//               } else if (ch === "bosstile") {
+//                   this.platforms.push((new Platform(AM.getAsset("./js/img/textures.png"), this, i * 50, j * 50, 50, 50, "B")));
+//               } else if (ch === "boss1block" && !this.treeBossDead) {
+//                   var mult = 1;
+//                   while (j + mult < currLevel.grid.length && currLevel.grid[j+mult][i] === "boss1block") {
+//                     currLevel.grid[j+mult][i] = "used_boss1block";
+//                     mult += 1;
+//                   }
+//                   this.platforms.push((new Platform(AM.getAsset("./js/img/textures.png"), this, i * 50, j * 50, 50, 50 * mult, "B")));
+//               } else if (ch === "boss2block") {
+//                   this.platforms.push((new Platform(AM.getAsset("./js/img/textures.png"), this, i * 50, j * 50, 50, 50, "B")));
+//               } else if (ch === "boss3block") {
+//                   this.platforms.push((new Platform(AM.getAsset("./js/img/textures.png"), this, i * 50, j * 50, 50, 50, "B")));
+//               } else if (ch === "used_boss1block") {
+//                 currLevel.grid[j][i] = "boss1block";
+//               } else if (ch === "used_boss2block") {
+//                 currLevel.grid[j][i] = "boss2block";
+//               } else if (ch === "used_boss3block") {
+//                 currLevel.grid[j][i] = "boss3block";
+//               }
+//               else if (ch === "password") {
+//                     saveRoom = true;
+//                     saveI = i;
+//                     saveJ = j
+//               }
+// =======
             ch = currLevel.grid[j][i];
 
             /************************
@@ -287,13 +419,32 @@ GameEngine.prototype.setLevel = function(exitedFrom) {
                 this.exits.push(new Portal(AM.getAsset("./js/img/textures.png"), this, i*50, j*50, 50, 50, "portal", "World 2"));
             } else if (ch === "portal3") {
                 this.exits.push(new Portal(AM.getAsset("./js/img/textures.png"), this, i*50, j*50, 50, 50, "portal", "World 3"));
+            } else if (ch === "portal4") {
+                this.exits.push(new Portal(AM.getAsset("./js/img/textures.png"), this, i*50, j*50, 50, 50, "portal", "Final Boss"));
             } else if (ch === "bosstile") {
                 this.platforms.push((new Platform(AM.getAsset("./js/img/textures.png"), this, i * 50, j * 50, 50, 50, "B")));
+            } else if (ch === "boss1block" && !this.treeBossDead) {
+                var mult = 1;
+                while (j + mult < currLevel.grid.length && currLevel.grid[j+mult][i] === "boss1block") {
+                  currLevel.grid[j+mult][i] = "used_boss1block";
+                  mult += 1;
+                }
+                this.platforms.push((new Platform(AM.getAsset("./js/img/textures.png"), this, i * 50, j * 50, 50, 50 * mult, "B")));
+            } else if (ch === "boss2block") {
+                this.platforms.push((new Platform(AM.getAsset("./js/img/textures.png"), this, i * 50, j * 50, 50, 50, "B")));
+            } else if (ch === "boss3block") {
+                this.platforms.push((new Platform(AM.getAsset("./js/img/textures.png"), this, i * 50, j * 50, 50, 50, "B")));
+            } else if (ch === "used_boss1block") {
+              currLevel.grid[j][i] = "boss1block";
+            } else if (ch === "used_boss2block") {
+              currLevel.grid[j][i] = "boss2block";
+            } else if (ch === "used_boss3block") {
+              currLevel.grid[j][i] = "boss3block";
             }
             else if (ch === "password") {
-                saveRoom = true;
-                saveI = i;
-                saveJ = j
+                  saveRoom = true;
+                  saveI = i;
+                  saveJ = j
             }
         }
     }
@@ -453,9 +604,9 @@ GameEngine.prototype.addBackgroundImage = function(image) {
 }
 
 GameEngine.prototype.draw = function () {
-    if (this.currentWorld.currentRoom === this.currentWorld.rooms[2][4] && this.treeBossDead) {
-        this.finished = true;
-    }
+    // if (this.currentWorld.currentRoom === this.currentWorld.rooms[2][4] && this.treeBossDead) {
+    //     this.finished = true;
+    // }
     //if (this.running === false && this.treeBossDead) {
     //var endscreen = new PlayGame(gameEngine, 300, 300);
     //endscreen.draw(this.ctx);
@@ -542,34 +693,60 @@ GameEngine.prototype.draw = function () {
         this.ctx.fillText("Use Arrow Keys to move!                                                                      Z = Jump    X = Shoot      Space = Toggle Powers (when available)       M = View Map" , 100, 100);
         //this.ctx.fillText("Z = Jump", 100, 100);
     }
-    if (this.finished) {
-        //this.ctx.drawImage(this.bg, 0, 0, 800, 650, 0, 0, 800, 650);
-        this.ctx.font = "  24pt Impact";
-        this.ctx.fillStyle = "red";
-
-        this.ctx.fillText("You survived the Tree Boss and earned a speed boost!", this.camera.xView + 75, this.camera.yView + 100);
-        this.ctx.fillText("Use new powers to explore new areas in the full game!", this.camera.xView + 75, this.camera.yView + 300);
-        this.ctx.fillText("Coming Soon!", this.camera.xView + 340, this.camera.yView + 400);
-    }
+    // if (this.finished) {
+    //     //this.ctx.drawImage(this.bg, 0, 0, 800, 650, 0, 0, 800, 650);
+    //     this.ctx.font = "  24pt Impact";
+    //     this.ctx.fillStyle = "red";
+    //
+    //     this.ctx.fillText("You survived the Tree Boss and earned a speed boost!", this.camera.xView + 75, this.camera.yView + 100);
+    //     this.ctx.fillText("Use new powers to explore new areas in the full game!", this.camera.xView + 75, this.camera.yView + 300);
+    //     this.ctx.fillText("Coming Soon!", this.camera.xView + 340, this.camera.yView + 400);
+    // }
     if (this.map) {
-        this.ctx.fillStyle = "green";
-        for (var i = 0; i < 10; i++) {
-            for (var j = 0; j < 10; j++) {
+      this.ctx.fillStyle = "green";
+      for (var i = 0; i < 10; i++) {
+        for (var j = 0; j < 10; j++) {
 
-                if (this.currentWorld.rooms[j][i] === this.currentWorld.currentRoom) {
-                    this.ctx.fillStyle = "yellow";
-                } else if (this.currentWorld.rooms[j][i].visited === true) {
-                    this.ctx.fillStyle = "blue";
-                } else if (this.currentWorld.rooms[j][i].visited === false) {
-                    this.ctx.fillStyle = "gray";
-                } else if (!(this.currentWorld.rooms[j][i] instanceof Level)) {
-                    this.ctx.fillStyle = "black";
-                }
+          if (this.currentWorld.rooms[j][i] === this.currentWorld.currentRoom) {
+            this.ctx.fillStyle = "yellow";
+          } else if (this.currentWorld.rooms[j][i].visited === true) {
+             this.ctx.fillStyle = "blue";
+           } else if (this.currentWorld.rooms[j][i].visited === false) {
+             this.ctx.fillStyle = "gray";
+           } else if (!(this.currentWorld.rooms[j][i] instanceof Level)) {
+             this.ctx.fillStyle = "black";
+          }
 
-                this.ctx.fillRect(this.camera.xView +275 + (i * 25) ,this.camera.yView + 200 + (j * 25) , 25, 25);
-            }
+          this.ctx.fillRect(this.camera.xView +275 + (i * 25) ,this.camera.yView + 200 + (j * 25) , 25, 25);
+          if (this.currentWorld.rooms[j][i].portalRoom) {
+            //console.log("PORTAAAALS");
+            this.ctx.drawImage(AM.getAsset("./js/img/textures.png"),
+                0, 200,  // source from sheet
+                50, 50,
+                this.camera.xView +275 + (i * 25), this.camera.yView + 200 + (j * 25),
+                25,
+                25);
+                //this.y += 50;
+          } else if (this.currentWorld.rooms[j][i].bossRoom) {
+            this.ctx.drawImage(AM.getAsset("./js/img/textures.png"),
+                0, 300,  // source from sheet
+                50, 50,
+                this.camera.xView +275 + (i * 25), this.camera.yView + 200 + (j * 25),
+                25,
+                25);
+
+          } else if (this.currentWorld.rooms[j][i].saveRoom) {
+            //MAKE SAVE TILE
+            // this.ctx.drawImage(AM.getAsset("./js/img/textures.png"),
+            //     0, 300,  // source from sheet
+            //     50, 50,
+            //     this.camera.xView +275 + (i * 25), this.camera.yView + 200 + (j * 25),
+            //     25,
+            //     25);
+          }
         }
     }
+  }
     this.ctx.restore();
 }
 
